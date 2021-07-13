0216057e9d7f78db341fe34526e14072f54e4076
lyw615 <shwwgrshbd@qq.com>
2021-07-13 11:01:18 +0800

    增加对对象宽高比的可视化分析

--- data_analyse/data_analyse.py
+++ data_analyse/data_analyse.py
@@ -49,10 +49,10 @@ def visual_image_wh(xml_dir, xmls=None, plot_type='points'):
         for p in fp:
 
             if '<size>' in p:
-                rectangle = [round(eval(next(fp).split('>')[1].split('<')[0])) for _ in range(2)]
+                size = [round(eval(next(fp).split('>')[1].split('<')[0])) for _ in range(2)]
 
-                height_width.append([rectangle[1], rectangle[0]])
-                hxw.append("%s*%s" % (rectangle[0], rectangle[1]))
+                height_width.append([size[1], size[0]])
+                hxw.append("%s*%s" % (size[1], size[0]))
 
         fp.close()
 
@@ -67,7 +67,11 @@ def visual_image_wh(xml_dir, xmls=None, plot_type='points'):
             value2count = value2count.transpose([1, 0])
 
             index_big2low = np.argsort(value2count[:, 1])[::-1]  # 获取从小到大排序的索引，并将索引反序
-            index_big2low = index_big2low[:15]  # 显示太多容易放不下
+
+            number_limit = 15
+            if len(index_big2low) > number_limit:
+                index_big2low = index_big2low[:number_limit]  # 显示太多容易放不下
+
             saved_value2count = value2count[index_big2low]
 
             saved_value2count = saved_value2count[np.argsort(saved_value2count[:, 0])]  # 显示的时候x坐标升序
@@ -139,6 +143,73 @@ def analyse_obs_per_image(names_resource, xml_dir=None):
     array_y = unique_count
 
     wh_dataframe = pd.DataFrame(array_y, columns=["obs num"])  # 实际还是调的matplotlib可视化
+    ax = wh_dataframe.plot(kind='bar', color="#55aacc")
+    ax.set_xticklabels(array_x, rotation=0)
+    plt.show()
+
+
+def analyse_obs_w2h_ratio(names_resource, xml_dir=None):
+    """
+       分析数据集中所有对象的宽高比，如果提供的names_resource是csv文件，
+       则同时需要提供xml文件夹路径，如果提供的是xml文件夹路径，那么赋给第一个参数就行
+    """
+    from collections import Counter
+    if os.path.isfile(names_resource):
+        "如果把xml文件名存在txt或者csv文件里"
+        with open(names_resource, 'r', encoding="utf-8") as f:
+            names = f.readlines()
+    elif os.path.isdir(names_resource):
+        "如果提供xml文件夹路径"
+        names = os.listdir(names_resource)
+        xml_dir = names_resource
+
+    rectangle_position = []
+    for _ in tqdm(names):
+
+        xml_path = os.path.join(xml_dir, _)
+        fp = open(xml_path)
+
+        for p in fp:
+            # if '<size>' in p:
+            #     width,height = [round(eval(next(fp).split('>')[1].split('<')[0])) for _ in range(2)]
+            if '<bndbox>' in p:
+                rectangle = []
+                [rectangle.append(round(eval(next(fp).split('>')[1].split('<')[0]))) for _ in range(4)]
+                rectangle_position.append(rectangle)
+
+        fp.close()
+
+    assert len(rectangle_position) > 0, "xml文件里没有找到对象信息"
+
+    # 计算所有的宽高比
+    rectangle_position = np.array(rectangle_position)
+    rec_width = rectangle_position[:, 2] - rectangle_position[:, 0]
+    rec_height = rectangle_position[:, 3] - rectangle_position[:, 1]
+    w2h_ratio = rec_width / rec_height
+
+    w2h_ratio = w2h_ratio.astype(np.int)
+    w2h_ratio = dict(Counter(w2h_ratio))  # 计算给ratio出现的频次
+
+    values = np.array(list(w2h_ratio.values()))
+    keys = np.array(list(w2h_ratio.keys()))
+    value_big = np.argsort(values)[::-1]
+
+    number_limit = 15
+    if len(value_big) > number_limit:
+        value_big = value_big[:number_limit]  # 只统计频数前15
+
+        keys = keys[value_big]
+        values = values[value_big]
+
+    keys_ind = np.argsort(keys)  # 按ratio的值排序
+    keys = keys[keys_ind]
+    values = values[keys_ind]
+
+    "宽高比的值为x轴，对应的数量为y轴"
+    array_x = keys
+    array_y = values
+
+    wh_dataframe = pd.DataFrame(array_y, columns=["w2h_ratio"])  # 实际还是调的matplotlib可视化
     ax = wh_dataframe.plot(kind='bar', color="#55aacc")
     ax.set_xticklabels(array_x, rotation=0)
     plt.show()
