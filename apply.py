9fbe3b430821527300894c65d4d9f3d9667723bc
lyw615 <shwwgrshbd@qq.com>
2021-07-13 20:42:57 +0800

    增加了从voc格式的xml文件里可视化label

--- apply.py
+++ apply.py
@@ -4,5 +4,9 @@ file_path = os.path.abspath(__file__)
 sys.path.append(os.path.abspath(os.path.join(file_path, "..", "..")))
 
 from code_aculat.data_analyse.data_analyse import analyse_image_wh
-
-analyse_image_wh(r"/home/data1/GeoAI_Data/compete_unzip/train/Annotations", [], plot_type='histogram')
+from code_aculat.visualize.visualize_voc_xml import  visualize_voc_xml
+# analyse_image_wh(r"/home/data1/GeoAI_Data/compete_unzip/train/Annotations", [], plot_type='histogram')
+visualize_voc_xml(r"E:\push_branch\resources_ml\out\training\object_ext_train_data\Annotations\new_anno",
+                  r"E:\push_branch\resources_ml\out\training\object_ext_train_data\new_Images",
+                  image_suffix='.tif'
+                  )
---
+++ visualize/visualize_voc_xml.py
@@ -0,0 +1,26 @@
+import sys, os
+
+file_path = os.path.abspath(__file__)
+sys.path.append(os.path.abspath(os.path.join(file_path, "..", "..")))
+from code_aculat.utils.xml_process import analyze_xml
+from code_aculat.visualize.visual_base import draw_bboxes_on_image
+
+import numpy as np
+
+
+def visualize_voc_xml(xml_source, image_dir, image_suffix='.tif'):
+    assert os.path.isdir(xml_source) or os.path.isfile(xml_source), "please assign xml_source"
+    if os.path.isdir(xml_source):
+        xmls = os.listdir(xml_source)
+        xmls = [os.path.join(xml_source, xml) for xml in xmls]
+
+    for _ in range(len(xmls)):
+        if np.random.randint(1, 10) > 7:  # 1/3的概率会被可视化
+
+            # 从xml获取类别名和bbox坐标
+            class_names, rectangles = analyze_xml(xmls[_])
+
+            # 在图像上绘制
+            image_name = os.path.basename(xmls[_]).replace('.xml', image_suffix)
+            image_path = os.path.join(image_dir, image_name)
+            draw_bboxes_on_image(image_path, rectangles, class_names)
