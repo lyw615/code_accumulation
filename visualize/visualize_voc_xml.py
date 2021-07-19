8ae42ca7de46a58aabcb2d5fa7bb1b313a6c2ec9
lyw615 <shwwgrshbd@qq.com>
2021-07-19 12:01:28 +0000

    visual big size data

--- visualize/visualize_voc_xml.py
+++ visualize/visualize_voc_xml.py
@@ -9,18 +9,19 @@ import numpy as np
 
 
 def visualize_voc_xml(xml_source, image_dir, image_suffix='.tif', show_pro=0.7):
-    assert os.path.isdir(xml_source) or os.path.isfile(xml_source), "please assign xml_source"
-    if os.path.isdir(xml_source):
-        xmls = os.listdir(xml_source)
-        xmls = [os.path.join(xml_source, xml) for xml in xmls]
+    # xml_source: dir,file,list
+    if isinstance(xml_source, str):
+        if os.path.isdir(xml_source):
+            xmls = os.listdir(xml_source)
+            xml_source = [os.path.join(xml_source, xml) for xml in xmls]
 
-    for _ in range(len(xmls)):
+    for _ in range(len(xml_source)):
         if np.random.randint(1, 1000) / 1000 < show_pro:  # 1/3的概率会被可视化
 
             # 从xml获取类别名和bbox坐标
-            class_names, rectangles = analyze_xml(xmls[_])
+            class_names, rectangles = analyze_xml(xml_source[_])
 
             # 在图像上绘制
-            image_name = os.path.basename(xmls[_]).replace('.xml', image_suffix)
+            image_name = os.path.basename(xml_source[_]).replace('.xml', image_suffix)
             image_path = os.path.join(image_dir, image_name)
             draw_bboxes_on_image(image_path, rectangles, class_names)
